91325724c33ad68fa0174608890cffb7a7f58b1d
Ian McKenney <ian.jafool@gmail.com>
2015-09-11 10:35:13 -0700

    pass 3rd spec

--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('findReplace', function() {
     it('replaces multiple instances of a word with another word', function() {
         expect(findReplace('hello world world', 'world', 'Gerald')).to.equal('hello Gerald Gerald');
     });
+
+    it('replaces all instances of a word even if it has capitals or not', function() {
+        expect(findReplace('hello World world', 'world', 'Gerald')).to.equal('hello Gerald Gerald');
+    });
 });
