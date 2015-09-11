ea584c377a1a3a148bd0eb6f47927e215df442c0
Ian McKenney <ian.jafool@gmail.com>
2015-09-11 10:43:11 -0700

    pass 4th spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 var findReplace = function(string, word, newWord) {
+    word = word.replace(/([.?*+^$[\]\\()|-])/g, "\\$1");
+    // debugger;
     var regex = new RegExp(word, "gi");
     var result = string.replace(regex, newWord);
 
