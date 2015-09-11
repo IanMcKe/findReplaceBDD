a24098aa9e9ede41070afa61a06cfda5dd4595c5
Ian McKenney <ian.jafool@gmail.com>
2015-09-11 10:32:19 -0700

    pass first and second specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-var findReplace = function(string, word, replace) {
-    
+var findReplace = function(string, word, newWord) {
+    var regex = new RegExp(word, "gi");
+    var result = string.replace(regex, newWord);
+
+    return result;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('findReplace', function() {
     it('replaces one instance of a word with another word', function() {
         expect(findReplace('hello world', 'world', 'Gerald')).to.equal('hello Gerald');
     });
+
+    it('replaces multiple instances of a word with another word', function() {
+        expect(findReplace('hello world world', 'world', 'Gerald')).to.equal('hello Gerald Gerald');
+    });
 });
